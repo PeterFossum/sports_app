3018d970427de94fbbc6137709017d2041959a7a
PeterFossum <pgfossum@gmail.com>
2016-07-23 23:15:19 -0700

    Finish layout and routes

---
+++ app/controllers/team_controller.rb
@@ -0,0 +1,4 @@
+class TeamController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
 
-  # Setting up a Root Route
   root 'static_pages#home'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'user#new'
+
+  get 'team/new'
 
 end
